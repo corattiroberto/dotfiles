ccf84c8cb07b52b0a488ca13c697b8c7e45a30df
Roberto Coratti <corattiroberto@gmail.com>
2024-07-20 19:08:50 +0200

    chore: code refactoring

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -8,8 +8,6 @@
     ../../modules/home/default.nix
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "korazza";
   home.homeDirectory = "/home/korazza";
 
@@ -24,19 +22,7 @@
     vscode.enable = true;
   };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
 
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }

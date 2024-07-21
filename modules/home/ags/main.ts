59a62f45591fec8d0bec39345c94546ee0ff4cf9
Roberto Coratti <corattiroberto@gmail.com>
2024-07-21 17:30:16 +0200

    feat(ags): add notifications

--- modules/home/ags/main.ts
+++ modules/home/ags/main.ts
@@ -2,7 +2,7 @@ import { Bar } from "./Bar/index.js"
 import { ScreenCorners } from "ScreenCorners/index.js"
 import { QuickSettings } from "./QuickSettings/index.js"
 import { PowerMenu } from "PowerMenu/index.js"
-//import { Notifications } from "./notifications.js";
+import { Notifications } from "./Notifications/index.js";
 import { inAllMonitors } from "./utils.js"
 import { AppLauncher } from "AppLauncher/index.js"
 
@@ -11,7 +11,7 @@ App.config({
   windows: [
     ...inAllMonitors(Bar),
     ...inAllMonitors(ScreenCorners),
-    //    ...inAllMonitors(Notifications),
+    ...inAllMonitors(Notifications),
     QuickSettings(),
     PowerMenu(),
     AppLauncher(),
